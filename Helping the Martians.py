090d4f249606afaa8550d72554809d0d9315af1d
Kanybekova1 <150647176+Kanybekova1@users.noreply.github.com>
2023-11-30 00:39:18 +0600

    Add files via upload

---
+++ Helping the Martians.py
@@ -0,0 +1,45 @@
+import random
+
+def initialize_cargo():
+    return [
+        {"location": random.randint(1, 7), "weight": 200},
+        {"location": random.randint(1, 7), "weight": 310},
+        {"location": random.randint(1, 7), "weight": 203}
+    ]
+
+def move_cargo(cargo):
+    for item in cargo:
+        item["location"] = random.randint(1, 7)
+
+def check_total_weight(found_cargo):
+    total_weight = sum(item["weight"] for item in found_cargo)
+    return total_weight == 713
+
+def main():
+    cargo = initialize_cargo()
+    found_cargo = []
+
+    print("")
+    print("Please input location coordinates to find the cargo (1-7).")
+
+    while len(found_cargo) < 3:
+        move_cargo(cargo)
+
+        for i in range(3):
+            location = int(input(f"Enter the location coordinate {i + 1}: "))
+            for i in cargo:
+                if i ["location"] == location:
+                    print(f"Cargo found at location {location} with a weight of {i['weight']} units.")
+                    found_cargo.append(i)
+                    cargo.remove(i)
+                    break
+
+        if not check_total_weight(found_cargo):
+            print("The total weight is not 713 units. Cargo will be relocated.")
+            found_cargo = []
+            cargo = initialize_cargo()
+
+    print("Congratulations! All cargo has been located!")     
+
+if __name__ == "__main__":
+    main()
